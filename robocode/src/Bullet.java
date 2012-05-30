bda26449676c8d6af8ec51255711cdfeccd93c71
abenson <abenson@systemsinmotion.com>
2012-05-30 17:47:36 -0400

    new Bullet and BulletQueue classes

---
+++ robocode/src/Bullet.java
@@ -0,0 +1,19 @@
+
+public class Bullet {
+	
+	double x;
+	double y;
+	double direction;
+	double power;
+	long time;
+	
+	public Bullet(double x, double y, double direction, double power, long l) {
+		this.x = x;
+		this.y = y;
+		this.direction = direction;
+		this.power = power;
+		this.time = time;
+	}
+
+	
+}
---
+++ robocode/src/BulletQueue.java
@@ -0,0 +1,8 @@
+import java.util.LinkedList;
+
+
+@SuppressWarnings("serial")
+public class BulletQueue extends LinkedList<Bullet>{
+
+
+}
