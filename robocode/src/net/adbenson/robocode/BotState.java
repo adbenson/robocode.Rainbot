f33b503cf3f43f885ba79874d224f7dfa9eba5ce
abenson <abenson@systemsinmotion.com>
2012-05-31 18:01:18 -0400

    Basically working

--- robocode/src/net/adbenson/robocode/BotState.java
+++ robocode/src/net/adbenson/robocode/BotState.java
@@ -1,5 +1,73 @@
 package net.adbenson.robocode;
 
-public interface BotState {
+import net.adbenson.utility.Vector;
+import robocode.AdvancedRobot;
+import robocode.ScannedRobotEvent;
+
+public abstract class BotState<T extends BotState<T>> {
+	
+	public final String name;
+	public final double energy;
+	public final double heading;
+	public final double velocity;
+	
+	public Vector position;
+	
+	final T previous;
+	final T change;
+	
+	protected BotState(String name, double energy, double heading, double velocity, T previous) {
+		this.name = name;
+		this.energy = energy;
+		this.heading = heading;
+		this.velocity = velocity;
+		
+		this.previous = previous;
+		if (previous == null) {
+			change = null;
+		}
+		else {
+			change = this.diff(previous);
+		}
+	}
+
+	public BotState(ScannedRobotEvent bot) {
+		this(
+			bot.getName(),
+			bot.getEnergy(),
+			bot.getHeading(),
+			bot.getVelocity(),
+			null
+		);
+	}
+	
+	public BotState(AdvancedRobot bot) {
+		this(
+				bot.getName(),
+				bot.getEnergy(),
+				bot.getHeading(),
+				bot.getVelocity(),
+				null
+		);
+	}
+	
+	public Vector getPosition() {
+		return position;
+	}
+	
+	protected void setPosition(Vector position) {
+		this.position = position;
+	}
+	
+	public abstract T diff(T previous);
+	
+	/**
+	 * Guess if the bot has just stopped
+	 * @return true if the bot was moving, and now is not.
+	 */
+	public boolean stopped() {
+		//Check if the bot's current speed is very low, and it wasn't before
+		return (Math.abs(velocity) < 0.01 && Math.abs(change.velocity) > 0.01);
+	}
 
 }
