90181faaf521b4ddaaff153d8a8c4d0a7e4a3a8e
abenson <abenson@systemsinmotion.com>
2012-05-31 18:00:50 -0400

    Perhaps completed?

--- robocode/src/net/adbenson/robocode/OpponentState.java
+++ robocode/src/net/adbenson/robocode/OpponentState.java
@@ -1,18 +1,60 @@
 package net.adbenson.robocode;
+import net.adbenson.utility.Vector;
 import robocode.ScannedRobotEvent;
 
-@SuppressWarnings("serial")
-public abstract class OpponentState extends ScannedRobotEvent implements BotState {
+public class OpponentState extends BotState<OpponentState> {
+	
+	final double bearing;
+	final double distance;
 
-	public OpponentState(ScannedRobotEvent event) {
-		// ScannedRobotEvent(String name, double energy, double bearing, double distance, double heading, double velocity)
-		super(event.getName(), event.getEnergy(), event.getBearingRadians(),
-				event.getDistance(), event.getHeading(), event.getVelocity());
+	public OpponentState(ScannedRobotEvent event, SelfState self) {
+		this(event, null, self);
 	}
 
-	public OpponentState(String string, double d, double e, double f, double g, double h) {
-		// ScannedRobotEvent(String name, double energy, double bearing, double distance, double heading, double velocity)
-		super(string, d, e, f, g, h);
+	public OpponentState(ScannedRobotEvent current, OpponentState previous, SelfState self) {
+		this(current.getName(), current.getEnergy(), current.getHeading(), current.getVelocity(), 
+				current.getBearingRadians(), current.getDistance(), previous, self);
+	}	
+
+	public OpponentState(String name, double energy, double heading, double velocity, 
+			double bearing, double distance, OpponentState previous, SelfState self) {
+		super(name, energy, heading, velocity, previous);
+		
+		this.bearing = bearing;
+		this.distance = distance;
+		
+		if (previous != null) {
+			this.setPosition(calculatePosition(self));
+		}
+	}
+
+	private Vector calculatePosition(SelfState self) {
+		double absoluteBearing = self.heading + this.bearing;
+		
+		Vector relative = Vector.getVectorFromAngle(absoluteBearing, distance);
+
+		return relative.add(self.getPosition());
+	}
+
+
+	@Override
+	public OpponentState diff(OpponentState previous) {
+		OpponentState diff = new OpponentState(
+				this.name + "_DIFF",
+				previous.energy - this.energy,
+				previous.heading - this.heading,
+				previous.velocity - this.velocity,
+				previous.bearing - this.bearing,
+				previous.velocity - this.velocity,
+				null,
+				null
+			);
+		
+		if (previous != null) {
+			diff.setPosition(previous.position.subtract(this.position));
+		}
+		
+		return diff;
 	}
 
 }
