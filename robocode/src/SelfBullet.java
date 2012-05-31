2b3c4431d693a75b999c6dadb4ae1189f5fa4ec6
abenson <abenson@systemsinmotion.com>
2012-05-31 12:58:15 -0400

    Placeholder methods

--- robocode/src/SelfBullet.java
+++ robocode/src/SelfBullet.java
@@ -1,3 +1,4 @@
+import java.awt.Graphics2D;
 import java.awt.geom.Point2D;
 
 
@@ -8,6 +9,24 @@ public class SelfBullet extends Bullet {
 	public SelfBullet(Point2D origin, double direction, double power, long time) {
 		super(origin, power, time);
 		this.direction = direction;
+	}
+
+	@Override
+	public void draw(Graphics2D g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean shouldDelete() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void updateProjection() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
