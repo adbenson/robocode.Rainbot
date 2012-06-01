a29fdc0af2d567bca146ee40674d7503be641d41
abenson <abenson@systemsinmotion.com>
2012-06-01 17:00:01 -0400

    Useful stuff; especially angle difference

--- robocode/src/net/adbenson/utility/Utility.java
+++ robocode/src/net/adbenson/utility/Utility.java
@@ -5,6 +5,9 @@ import java.awt.Graphics2D;
 
 public class Utility {
 	
+	public static final double TWO_PI = Math.PI * 2d;
+	public static final double HALF_PI = Math.PI / 2d;
+	
 	public static Color setAlpha(Color c, double alpha) {
 		return new Color(c.getRed(), c.getGreen(), c.getBlue(), (int)(alpha * 255));
 	}
@@ -21,6 +24,19 @@ public class Utility {
 		g.drawLine(x, y-start, x, y-end);
 		g.drawLine(x+start, y, x+end, y);
 		g.drawLine(x, y+start, x, y+end);
+	}
+	
+	public static double angleDifference(double a, double b) {
+		double diff = a-b;
+		
+		if (diff > Math.PI) {
+			diff -= TWO_PI;
+		}
+		else if (diff < -Math.PI) {
+			diff += TWO_PI;
+		}
+		
+		return diff;
 	}
 
 }
