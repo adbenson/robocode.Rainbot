779de65d719d3b974bb1becddedf2e2da7d64b8c
Drew Benson <abenson@systemsinmotion.com>
2012-06-05 15:56:23 -0400

    Added message to Impossible Future

--- robocode/src/net/adbenson/robocode/prediction/ImpossibleToSeeTheFutureIsException.java
+++ robocode/src/net/adbenson/robocode/prediction/ImpossibleToSeeTheFutureIsException.java
@@ -3,4 +3,7 @@ package net.adbenson.robocode.prediction;
 @SuppressWarnings("serial")
 public class ImpossibleToSeeTheFutureIsException extends Exception {
 
+	public ImpossibleToSeeTheFutureIsException(String msg) {
+		super(msg);
+	}
 }
