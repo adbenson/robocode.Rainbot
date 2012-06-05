22398d70d406c1f29f7e718c1cef982ed13e0016
Drew Benson <abenson@systemsinmotion.com>
2012-06-05 17:50:12 -0400

    Adding comparator based on turns to hit

--- robocode/src/net/adbenson/robocode/prediction/PredictedTarget.java
+++ robocode/src/net/adbenson/robocode/prediction/PredictedTarget.java
@@ -7,17 +7,29 @@ import net.adbenson.robocode.botstate.OpponentState;
 public class PredictedTarget {
 	
 	public final double requiredPower;
-	
 	public final OpponentState target;
+	public final int turnsToPosition;
 	
 	protected static final Comparator<PredictedTarget> powerComparator = new TargetPowerComparator();
+	protected static final Comparator<PredictedTarget> turnsComparator = new TurnsToPositionComparator();
 
-	protected PredictedTarget(double requiredPower, OpponentState target) {
+	protected PredictedTarget(double requiredPower, OpponentState target, int turnsToPosition) {
 		this.requiredPower = requiredPower;
 		this.target = target;
+		this.turnsToPosition = turnsToPosition;
 	}
 	
 	public static class TargetPowerComparator implements Comparator<PredictedTarget> {
+
+		@Override
+		public int compare(PredictedTarget o1, PredictedTarget o2) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+		
+	}
+	
+	public static class TurnsToPositionComparator implements Comparator<PredictedTarget> {
 
 		@Override
 		public int compare(PredictedTarget o1, PredictedTarget o2) {
