417519fbf5d5433bfbd9c2241f9a0ad73878d36b
Drew Benson <abenson@systemsinmotion.com>
2012-06-05 15:44:47 -0400

    Added comparator for sorting targets by firepower

--- robocode/src/net/adbenson/robocode/prediction/PredictedTarget.java
+++ robocode/src/net/adbenson/robocode/prediction/PredictedTarget.java
@@ -1,5 +1,7 @@
 package net.adbenson.robocode.prediction;
 
+import java.util.Comparator;
+
 import net.adbenson.robocode.botstate.OpponentState;
 
 public class PredictedTarget {
@@ -7,10 +9,22 @@ public class PredictedTarget {
 	public final double requiredPower;
 	
 	public final OpponentState target;
+	
+	protected static final Comparator<PredictedTarget> powerComparator = new TargetPowerComparator();
 
 	protected PredictedTarget(double requiredPower, OpponentState target) {
 		this.requiredPower = requiredPower;
 		this.target = target;
+	}
+	
+	public static class TargetPowerComparator implements Comparator<PredictedTarget> {
+
+		@Override
+		public int compare(PredictedTarget o1, PredictedTarget o2) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+		
 	}
 
 }
