f8f5e74e9087e5c4e1f648f7830c108816a3aab8
Drew Benson <abenson@systemsinmotion.com>
2012-06-05 10:19:55 -0400

    Added color coordination and self bullet tracking

--- robocode/src/net/adbenson/robocode/bullet/SelfBullet.java
+++ robocode/src/net/adbenson/robocode/bullet/SelfBullet.java
@@ -7,22 +7,38 @@ import net.adbenson.robocode.botstate.OpponentState;
 import net.adbenson.robocode.botstate.SelfState;
 import net.adbenson.robocode.rainbot.Rainbot;
 import net.adbenson.utility.Utility;
+import net.adbenson.utility.Vector;
 
 
 public class SelfBullet extends Bullet {
 	
+	private static final int COLOR_WHEEL = 5;
+	private static final float COLOR_RATIO = 1f / COLOR_WHEEL;
+	private static int nextColorIndex = 0;
+		
 	private final OpponentState target;
+	private final int colorIndex;
 	
 	public SelfBullet(SelfState self, long time, OpponentState target) {
 		super(self, self.gunHeading, time);
 		this.target = target;
+		this.colorIndex = (nextColorIndex++) % COLOR_WHEEL;
 	}
 
 	@Override
 	public void draw(Graphics2D g) {
 		g.setStroke(new BasicStroke(2));
-		g.setColor(Color.orange);
+
+		Color c = Color.getHSBColor(COLOR_RATIO * colorIndex, 1, 1);
+		g.setColor(Utility.setAlpha(c, 0.6));
+		
 		Utility.drawCrosshairs(g, target.position, 5, 40);
+		
+		Vector current = Vector.getVectorFromAngle(heading, getDistanceTravelled()).add(origin);
+		
+		origin.drawTo(g, heading, getDistanceTravelled());
+		
+		g.fillOval(current.intX(), current.intY(), 20, 20);
 	}
 
 	@Override
