c0cbad0313ae9a04332803a7d41b1ca3c1843546
Drew <abenson3@emich.edu>
2012-06-06 21:24:48 -0400

    Refactoring BattleHistory to track multiple opponents

--- robocode/src/net/adbenson/robocode/botstate/BattleHistory.java
+++ robocode/src/net/adbenson/robocode/botstate/BattleHistory.java
@@ -1,4 +1,8 @@
 package net.adbenson.robocode.botstate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
+
 import net.adbenson.robocode.bullet.BulletQueue;
 import net.adbenson.robocode.bullet.OpponentBullet;
 import net.adbenson.robocode.bullet.SelfBullet;
@@ -7,79 +11,114 @@ import robocode.ScannedRobotEvent;
 
 public class BattleHistory {
 	
-	private BulletQueue<OpponentBullet> opponentBullets;
 	private BulletQueue<SelfBullet> selfBullets;
+	private HashMap<String, BulletQueue<OpponentBullet>> opponentBullets;
 	
-	private BattleState currentState;
+	private SelfState selfState;
+	private HashMap<String, OpponentState> opponentStates;
 	
-	private int stateCount;
+	private long turn;
+	
+	private String targetName;
 	
 	public BattleHistory() {
 		super();
-		
-		opponentBullets = new BulletQueue<OpponentBullet>();
+
 		selfBullets = new BulletQueue<SelfBullet>();
+		opponentBullets = new HashMap<String, BulletQueue<OpponentBullet>>();
 		
-		currentState = null;
+		selfState = null;
+		opponentStates = new HashMap<String, OpponentState>();
 		
-		stateCount = 0;
+		turn = -1;
+		targetName = "";
 	}
 	
-	public void addBots(AdvancedRobot self, ScannedRobotEvent opp, long time) {
-		BattleState next;
+	public void addBots(AdvancedRobot self, LinkedList<ScannedRobotEvent> foundOpponents) {
+		this.turn = self.getTime();
 		
-		if (getCurrentState() != null) {
-			next = getCurrentState().nextBattleState(self, opp, time);
-		}
-		else {
-			next = new BattleState(self, opp, time);
-		}
+		selfState = new SelfState(self, selfState);
 		
-		currentState = next;
-		stateCount++;
+		for(ScannedRobotEvent opponent : foundOpponents) {
+			String name = opponent.getName();
+			OpponentState newOpponentState;
+			
+			if (opponentStates.containsKey(name)) {
+				OpponentState currentOpponent = opponentStates.get(name);
+				
+				newOpponentState = new OpponentState(opponent, currentOpponent, self);
+			}
+			else {
+				newOpponentState = new OpponentState(opponent, self);
+			}
+			
+			opponentStates.put(name, newOpponentState);
+		}
 	}
 
 	public BulletQueue<SelfBullet> getSelfBullets() {
 		return selfBullets;
 	}
 
-	public BulletQueue<OpponentBullet> getOpponentBullets() {
-		return opponentBullets;
-	}
-
-	public BattleState getCurrentState() {
-		return currentState;
+	public BulletQueue<OpponentBullet> getOpponentBullets(String name) {
+		return opponentBullets.get(name);
 	}
 	
-	public OpponentState getCurrentOpponent() {
-		return currentState.opponent;
+	public OpponentState getOpponent(String name) {
+		return opponentStates.get(name);
 	}
 
-	public void opponentFired() {
-		opponentBullets.add(
-				new OpponentBullet(
-						currentState.opponent,
-						currentState.time
-				)
-		);
+	public void opponentFired(String name, long turn) {
+		OpponentState opponent = opponentStates.get(name);
+		
+		OpponentBullet bullet = new OpponentBullet(opponent, turn);
+		
+		BulletQueue<OpponentBullet> queue;
+		
+		if (!opponentBullets.containsKey(name)) {
+			queue = new BulletQueue<OpponentBullet>();
+			opponentBullets.put(name, queue);
+		}
+		else {
+			queue = opponentBullets.get(name);
+		}
+		
+		queue.add(bullet);
 	}
 
-	public void selfFired(OpponentState candidateTarget, robocode.Bullet bullet) {
-		selfBullets.add(
-				new SelfBullet(
-						currentState.self,
-						bullet,
-						currentState.time,
-						candidateTarget
-				)
-		);
+	public void selfFired(OpponentState target, robocode.Bullet bullet, long turn) {
+		selfBullets.add(new SelfBullet(selfState, bullet, turn, target));
 	}
 
-	public boolean hasCurrentState() {
-		return stateCount >= 1;
+	public Collection<BulletQueue<OpponentBullet>> getAllOpponentBullets() {
+		return opponentBullets.values();
 	}
 
-	public int getStateCount() {
-		return stateCount;
+	public SelfState getSelf() {
+		return selfState;
+	}
+
+	public Collection<OpponentState> getAllOpponents() {
+		return opponentStates.values();
+	}
+
+	public boolean hasTarget() {
+		return opponentStates.containsKey(targetName);
+	}
+	
+	public long getTurn() {
+		return turn;
+	}
+	
+	public void setTargetName(String name) {
+		this.targetName = name;
+	}
+	
+	public String getTargetName() {
+		return targetName;
+	}
+	
+	public OpponentState getTarget() {
+		return opponentStates.get(targetName);
 	}
 }
