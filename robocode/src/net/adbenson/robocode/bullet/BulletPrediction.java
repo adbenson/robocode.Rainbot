faeb364f38ffd448a63e9e4716bb8fc2a588b75d
Drew Benson <abenson@systemsinmotion.com>
2012-06-12 15:37:47 -0400

    Starting on bullet prediction

---
+++ robocode/src/net/adbenson/robocode/bullet/BulletPrediction.java
@@ -0,0 +1,15 @@
+package net.adbenson.robocode.bullet;
+
+public class BulletPrediction {
+	
+	private BulletQueue<OpponentBullet> history;
+	
+	public BulletPrediction(BulletQueue<OpponentBullet> history) {
+		this.history = history;
+	}
+	
+	public void dodge(OpponentBullet b) {
+		
+	}
+
+}
